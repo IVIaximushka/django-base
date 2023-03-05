884f2109464b0e5ee9dc85d46c203007da98814e
Max <dmitmax2002@yandex.ru>
2023-03-05 18:34:49 +0300

    + registration view and form

--- web/views.py
+++ web/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from web.forms import RegistrationForm
+
 
 def main_view(request):
     return render(request, 'web/main.html')
+
+
+def registration_view(request):
+    form = RegistrationForm()
+    return render(request, 'web/registration.html', {'form': form})
