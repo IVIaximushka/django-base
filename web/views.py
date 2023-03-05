398628b6cd1f41f23c301ce13b025a4d38de3395
Max <dmitmax2002@yandex.ru>
2023-03-05 18:59:01 +0300

    + registration form validation

--- web/views.py
+++ web/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 from web.forms import RegistrationForm
 
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 
 def main_view(request):
     return render(request, 'web/main.html')
@@ -9,4 +14,13 @@ def main_view(request):
 
 def registration_view(request):
     form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email']
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
     return render(request, 'web/registration.html', {'form': form})
