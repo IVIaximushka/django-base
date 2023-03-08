770bed25327337882a13b14362676b28c55719de
Max <dmitmax2002@yandex.ru>
2023-03-08 18:28:28 +0300

    + displaying sql queries middleware

--- proreader/settings.py
+++ proreader/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'proreader.debug.SqlPrintingMiddleware'
 ]
 
 ROOT_URLCONF = 'proreader.urls'
