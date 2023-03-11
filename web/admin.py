2fc04e0924d7832a29c0cfc8f2153c2e9a109477
Max <dmitmax2002@yandex.ru>
2023-03-11 11:54:08 +0300

    + admin parameters

--- web/admin.py
+++ web/admin.py
@@ -4,7 +4,11 @@ from web.models import Book
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'genre', 'description', 'done')
+    list_display = ('id', 'title', 'author', 'genre', 'description', 'done', 'user')
+    search_fields = ('id', 'title')
+    list_filter = ('author', 'genre', 'done')
+    ordering = ('title',)
+    readonly_fields = ('done',)
 
 
 admin.site.register(Book, BookAdmin)
