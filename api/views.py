cccf428fc357f37505232a872ff7b666bf63ce16
Max <dmitmax2002@yandex.ru>
2023-03-11 20:59:07 +0300

    + drf authorization for all methods

--- api/views.py
+++ api/views.py
@@ -1,6 +1,4 @@
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from api.serializers import BookNoteSerializer
@@ -8,13 +6,12 @@ from web.models import Book
 
 
 @api_view(['GET'])
+@permission_classes([])
 def main_view(request):
     return Response({'status': 'ok'})
 
 
 @api_view(['GET'])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
 def book_notes_view(request):
     book_notes = Book.objects.all().select_related('user').prefetch_related('tags')
     serializer = BookNoteSerializer(book_notes, many=True)
